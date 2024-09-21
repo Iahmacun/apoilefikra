92498bdbaefc174c5f3e632a74a9076f3c9c8731
Lahmacun <57403555+Iahmacun@users.noreply.github.com>
2024-09-21 14:12:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// View engine olarak EJS ayarlandı
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
+// Statik dosyaları servis et
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const jokes = [
-  { title: ' Temel İstanbul ', file: 'joke1.mp3' },
+  { title: 'Temel İstanbul', file: 'joke1.mp3' },
   { title: 'Boks Maçı', file: 'joke2.mp3' },
-  { title: 'Temel VE Cemal', file: 'joke3.mp3' },
-  { title: 'Temel Kahveye ', file: 'joke4.mp3' },
+  { title: 'Temel ve Cemal', file: 'joke3.mp3' },
+  { title: 'Temel Kahveye', file: 'joke4.mp3' },
 ];
 
+// Anasayfa yönlendirmesi
 app.get('/', (req, res) => {
   res.render('index', { jokes: jokes });
 });
 
+// Vercel üzerinde dinleyeceğimiz port ayarı
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Sunucu çalışıyor: http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${port}`);
+});
