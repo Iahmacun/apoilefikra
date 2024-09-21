78de94da35a65eb9370be8bbace3a7de3b4d18a1
Lahmacun <57403555+Iahmacun@users.noreply.github.com>
2024-09-21 14:16:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// EJS'i görünüm motoru olarak ayarla
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
+// Görünüm dizinini ayarla (varsayılan olarak 'views' olarak ayarlandığı için gerek yok, ama açıkça belirtmek iyi olabilir)
+app.set('views', path.join(__dirname, 'views'));
+
+// Statik dosyaları servis et
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const jokes = [
-  { title: ' Temel İstanbul ', file: 'joke1.mp3' },
+  { title: 'Temel İstanbul', file: 'joke1.mp3' },
   { title: 'Boks Maçı', file: 'joke2.mp3' },
-  { title: 'Temel VE Cemal', file: 'joke3.mp3' },
-  { title: 'Temel Kahveye ', file: 'joke4.mp3' },
+  { title: 'Temel ve Cemal', file: 'joke3.mp3' },
+  { title: 'Temel Kahveye', file: 'joke4.mp3' },
 ];
 
+// Anasayfa yönlendirmesi
 app.get('/', (req, res) => {
   res.render('index', { jokes: jokes });
 });
 
+// Port ayarını yap ve sunucuyu başlat
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Sunucu çalışıyor: http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${port}`);
+});
